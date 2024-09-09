37888552e085aecc412a0701562af62a429e6d3a
NHacker0 <Jhoniversong@gmail.com>
2024-09-09 08:12:59 +0800

    Added create, edit, delete layout

--- obj/Debug/net8.0/TaksManagement_System.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaksManagement_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaksManagement_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40933528b980372202db0414d184737ce0a84d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1f5208400ff13a2c4720f62b144b13ee296c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaksManagement_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaksManagement_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
